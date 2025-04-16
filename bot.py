5242e57d15bf7bfc10cbfe17353a4be85f1f88f2
crnK420 <fckhypex@yahoo.de>
2025-04-16 22:16:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,44 @@
 import os
+import discord
+from deep_translator import GoogleTranslator
+from dotenv import load_dotenv
+from flask import Flask
+import threading
 
-import nextcord
-from nextcord.ext import commands
+# Webserver für Render
+app = Flask('')
 
-import config
+@app.route('/')
+def home():
+    return "I'm alive!"
 
+def run():
+    app.run(host='0.0.0.0', port=8080)
 
-def main():
-    # Allows privledged intents for monitoring members joining, roles editing, and role assignments
-    # These need to be enabled in the developer portal as well
-    intents = nextcord.Intents.default()
+# Starte Webserver in separatem Thread
+threading.Thread(target=run).start()
 
-    # Required in order to read messages (eg. prefix commands)
-    intents.message_content = True
+# Discord Bot
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-    # To enable the guilds priveleged intent:
-    # intents.guilds = True
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
-    # To enable the members priveleged intent:
-    # intents.members = True
+CHANNELS_TO_TRANSLATE = ["𝔸𝕝𝕝𝕘𝕖𝕞𝕖𝕚𝕟", "𝔸𝕟𝕜ü𝕟𝕕𝕚𝕘𝕦𝕟𝕘𝕖𝕟", "𝕋𝕣𝕒𝕕𝕖𝕟"]
 
-    # Set custom status to "Listening to ?help"
-    activity = nextcord.Activity(
-        type=nextcord.ActivityType.listening, name=f"{config.BOT_PREFIX}help"
-    )
+@client.event
+async def on_ready():
+    print(f'✅ Bot ist online als {client.user}')
 
-    bot = commands.Bot(
-        commands.when_mentioned_or(config.BOT_PREFIX),
-        intents=intents,
-        activity=activity,
-    )
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    # Get the modules of all cogs whose directory structure is ./cogs/<module_name>
-    for folder in os.listdir("cogs"):
-        bot.load_extension(f"cogs.{folder}")
+    if message.channel.name in CHANNELS_TO_TRANSLATE:
+        translated = GoogleTranslator(source='auto', target='en').translate(message.content)
+        await message.channel.send(f"🇬🇧 **Übersetzung:**\n> {translated}")
 
-    @bot.listen()
-    async def on_ready():
-        """When Discord is connected"""
-        assert bot.user is not None
-        print(f"{bot.user.name} has connected to Discord!")
-
-    # Run Discord bot
-    bot.run(config.DISCORD_TOKEN)
-
-
-if __name__ == "__main__":
-    main()
+client.run(TOKEN)
